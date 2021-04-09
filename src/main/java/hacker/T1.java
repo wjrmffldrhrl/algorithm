c24c52c132e88331f1a91745033fcbb3aeb12ccc
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-04-09 10:58:54 +0900

    hackerrank solve

---
+++ src/main/java/hacker/T1.java
@@ -0,0 +1,80 @@
+package hacker;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+class T1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int numbersCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> numbers = IntStream.range(0, numbersCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int queriesRows = Integer.parseInt(bufferedReader.readLine().trim());
+        int queriesColumns = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, queriesRows).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Long> result = Result.findSum(numbers, queries);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    static class Result {
+        public static List<Long> findSum(List<Integer> numbers, List<List<Integer>> queries) {
+            List<Long> answer = new ArrayList<>();
+
+
+
+
+            long[] sums = new long[numbers.size()];
+            for (int number : numbers) {
+
+            }
+
+            return answer;
+        }
+
+
+
+
+
+    }
+
+}
