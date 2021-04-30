3eb1915871764209c422b9ae0bb89aa74ebdfab6
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-04-30 18:32:11 +0900

    programmers 크래인 인형뽑기 게임

--- src/main/java/unsolved/P2749_java/P2749.java
+++ src/main/java/unsolved/P2749_java/P2749.java
@@ -2,19 +2,16 @@ package unsolved.P2749_java;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class P2749 {
 
 
-    public static int[] fibo = {1, 1, 1, 0};
 
     public static int[] dc(long N) {
 
-
         if (N == 1) {
-            return fibo;
+            return new int[]{1, 1, 0};
         }
         if (N % 2 == 0) {
             int[] a = dc(N / 2);
@@ -26,16 +23,21 @@ public class P2749 {
             int[] a = dc((N - 1) / 2);
             int[] b = dc((N - 1) / 2);
 
-            return mul(mul(a, b), fibo);
+            return mul(mul(a, b), new int[]{1, 1, 0});
 
         }
     }
 
     public static int[] mul(int[] a, int[] b) {
+
+        long l1 = (((long) a[0] * b[0]) + ((long) a[1] * b[1])) % 1000000;
+        long l2 = (((long) a[0] * b[1])   + ((long) a[1] * b[2])) % 1000000;
+        long l3 = (((long) a[1] * b[1])   + ((long) a[2] * b[2])) % 1000000;
+
         return new int[]{
-                (a[0] * b[0] + a[1] * b[1]) % 1000000,
-                (a[0] * b[1] + a[1] * b[2]) % 1000000,
-                (a[1] * b[1] + a[2] * b[2]) % 1000000
+                (int) l1,
+                (int) l2,
+                (int) l3
         };
     }
 
@@ -47,7 +49,7 @@ public class P2749 {
         if (N <= 2) {
             System.out.println(1);
         } else {
-            System.out.println(dc(N - 1)[0] % 1000000);
+            System.out.println(dc(N - 1)[0]);
         }
 
     }
---
+++ src/main/java/unsolved/programmers/P64061.java
@@ -0,0 +1,18 @@
+package unsolved.programmers;
+
+class Solution {
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+
+        System.out.println(solution(board, moves));
+
+
+
+    }
+}
