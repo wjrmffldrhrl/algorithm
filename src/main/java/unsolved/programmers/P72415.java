fb0295465ee43f323f84ab3d44616a2967c323e0
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-05-01 17:05:52 +0900

    progmammers 카드 짝 맞추기

---
+++ src/main/java/unsolved/programmers/P72415.java
@@ -0,0 +1,22 @@
+package unsolved.programmers;
+
+public class P72415 {
+    public static int solution(int[][] board, int r, int c) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         * [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]]	1	0	14
+         * [[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]]	0	1	16
+         */
+        int[][] board = {{1, 0, 0, 3}, {2, 0, 0, 0}, {0, 0, 0, 2}, {3, 0, 1, 0}};
+        int r = 1;  // 최초 행
+        int c = 0; // 최초 열
+
+
+    }
+}
+``
