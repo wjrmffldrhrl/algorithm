a9bcbfe3e54b642c418ee68b270db968b917a84f
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-05-13 23:13:45 +0900

    hacker test 1

--- src/main/java/hacker/T1.java
+++ src/main/java/hacker/T1.java
@@ -1,80 +1,69 @@
 package hacker;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.*;
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.util.Scanner;
+import java.util.Stack;
 
+class Parser {
+    static String isBalanced(String s) {
+        Stack<Character> middleBracket = new Stack<>();
+        Stack<Character> bigBracket = new Stack<>();
 
-class T1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        char popBracket;
+        for (char b : s.toCharArray()) {
+            switch (b) {
+                case '(':
+                    middleBracket.push(b);
+                    break;
+                case '{':
+                    bigBracket.push(b);
+                    break;
+                case ')':
+                    if (middleBracket.isEmpty()) {
+                        return "false";
+                    }
+                    if (middleBracket.pop() != '(') {
+                        return "false";
+                    }
 
-        int numbersCount = Integer.parseInt(bufferedReader.readLine().trim());
+                    break;
+                case '}':
+                    if (bigBracket.isEmpty()) {
+                        return "false";
+                    }
 
-        List<Integer> numbers = IntStream.range(0, numbersCount).mapToObj(i -> {
-            try {
-                return bufferedReader.readLine().replaceAll("\\s+$", "");
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        })
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        int queriesRows = Integer.parseInt(bufferedReader.readLine().trim());
-        int queriesColumns = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<List<Integer>> queries = new ArrayList<>();
-
-        IntStream.range(0, queriesRows).forEach(i -> {
-            try {
-                queries.add(
-                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                                .map(Integer::parseInt)
-                                .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        List<Long> result = Result.findSum(numbers, queries);
-
-        bufferedWriter.write(
-                result.stream()
-                        .map(Object::toString)
-                        .collect(joining("\n"))
-                        + "\n"
-        );
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
-
-    static class Result {
-        public static List<Long> findSum(List<Integer> numbers, List<List<Integer>> queries) {
-            List<Long> answer = new ArrayList<>();
+                    if (bigBracket.pop() != '{') {
+                        return "false";
+                    }
 
 
-
-
-            long[] sums = new long[numbers.size()];
-            for (int number : numbers) {
+                    break;
 
             }
-
-            return answer;
         }
 
-
-
-
+        if (!middleBracket.isEmpty() || !bigBracket.isEmpty()) {
+            return "false";
+        } else {
+            return "true";
+        }
 
     }
 
 }
+
+class Solution1 {
+
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNext()) {
+            System.out.println(parser.isBalanced(in.next()));
+        }
+
+        in.close();
+    }
+}
+
