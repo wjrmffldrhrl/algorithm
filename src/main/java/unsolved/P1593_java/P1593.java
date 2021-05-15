1f7e357ee2faccc8efb99b4b89ef83269985730e
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-05-16 00:30:13 +0900

    p1593 memory out

--- src/main/java/unsolved/P1593_java/P1593.java
+++ src/main/java/unsolved/P1593_java/P1593.java
@@ -15,35 +15,36 @@ public class P1593 {
         String W = br.readLine();
         String S = br.readLine();
 
-        int[] wordCount = new int[60];
+        int[] wordCount = new int[∫60];
         for (char word : W.toCharArray()) {
             wordCount[word - 'A']++;
         }
 
-
         int count = 0;
-        for (int i = 0; i < S.length() - g; i++) {
-            boolean isMaya = true;
-            int[] tmpWordCount =  new int[60];
-            System.arraycopy(wordCount, 0, tmpWordCount, 0, wordCount.length);
-
-            char[] charArray = S.substring(i, i + g).toCharArray();
-            for (char word : charArray) {
-                if (tmpWordCount[word - 'A'] == 0) {
-                    isMaya = false;
-                    break;
-                } else {
-                    tmpWordCount[word - 'A']--;
-                }
-            }
-
-            if (isMaya) {
+        for (int i = 0; i < s - g; i++) {
+            if (isMaya(g, S, wordCount, i)) {
                 count++;
             }
-
         }
 
         System.out.println(count);
+    }
+
+    private static boolean isMaya(int g, String S, int[] wordCount, int i) {
+        int[] tmpWordCount =  new int[60];
+
+        for (int j = 0; j < g; j++) {
+            tmpWordCount[S.charAt(i + j) - 'A']++;
+        }
+
+
+        for (int j = 0; j < tmpWordCount.length; j++) {
+            if (wordCount[j] != tmpWordCount[j]) {
+                return false;
+            }
+
+        }
+        return true;
     }
 
 }
